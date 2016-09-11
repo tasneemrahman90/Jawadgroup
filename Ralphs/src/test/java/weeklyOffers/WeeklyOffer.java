22924d22665b0ebb85c10ecb99cdcbe0701ecc57
Jawad Newaj <PlayBoy@Jawads-MacBook-Pro.local>
2016-09-11 12:42:39 -0700

    added Ralphs build

--- Ralphs/src/test/java/weeklyOffers/WeeklyOffer.java
+++ Ralphs/src/test/java/weeklyOffers/WeeklyOffer.java
@@ -3,6 +3,7 @@ import Common.Base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+import pageFactory.Locations;
 import pageFactory.WeeklyOffers;
 
 /**
@@ -12,9 +13,11 @@ public class WeeklyOffer extends Base {
     @Test
     public void test() throws InterruptedException {
         WeeklyOffers offers = PageFactory.initElements(driver, WeeklyOffers.class);
+        Locations locations = PageFactory.initElements(driver, Locations.class);
         scrollPageDown();
         sleepFor(2);
         offers.clickGetStarted();
+        locations.inputZipNEnter("90660");
         sleepFor(3);
     }
 }
